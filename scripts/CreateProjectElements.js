c2e0cb0ac9b6f0f53e69ed5770166cbffd46e9ac
Tom <tom.ille@gmx.de>
2021-04-06 17:57:01 +0200

    implement new json data model

--- scripts/CreateProjectElements.js
+++ scripts/CreateProjectElements.js
@@ -1,74 +1,91 @@
-var reposPath = "https://api.github.com/repos/JamesTheButler"
-    //"contents/githubio.json"
+var reposPath = "https://api.github.com/repos/JamesTheButler";
+//"contents/githubio.json"
 
-var projectsPath = "/JamesTheButler.github.io/contents/data/projects.json"
+var projectsPath = "/JamesTheButler.github.io/contents/data/";
 
 var startWithEven = 0;
 
 // generate a project element
-function CreateProjectHtml(contentObject, IsEven) {
-    if (IsEven)
-        var projectElementTemplate = document.getElementById("TemplateProjectEven");
-    else
-        var projectElementTemplate = document.getElementById("TemplateProjectOdd");
+function CreateProjectHtml(projectName, IsEven) {
+  var projectPath =
+    reposPath + projectsPath + "projects/" + projectName + ".json";
+  console.log(projectPath);
 
-    var projectElement = projectElementTemplate.content.cloneNode(true);
+  // pull and base64 decode the content-tag in the projects.json file from github
+  var projectJsonContent = JSON.parse(
+    atob(JSON.parse(HttpGet(projectPath)).content)
+  );
+  console.log(projectJsonContent);
 
-    projectElement.querySelector("#project_name").textContent = contentObject.project_name;
-    //GenerateTagItems_String(contentObject, mainDivClone);
-    GenerateTagItems_SOF_Style(contentObject, projectElement);
-    //console.log("1" + projectElement)
-    projectElement.querySelector("#project_description").textContent = contentObject.project_description;
-    projectElement.querySelector("#repo-image").src = contentObject.image_uri;
-    //console.log("2" + projectElement)
+  if (IsEven)
+    var projectElementTemplate = document.getElementById("TemplateProjectEven");
+  else
+    var projectElementTemplate = document.getElementById("TemplateProjectOdd");
 
-    GenerateLinkButtons(contentObject, projectElement);
-    document.getElementById("repo-content").appendChild(projectElement);
+  var projectElement = projectElementTemplate.content.cloneNode(true);
+
+  projectElement.querySelector("#project_name").textContent =
+    projectJsonContent.project_name;
+  //GenerateTagItems_String(contentObject, mainDivClone);
+  GenerateTagItems_SOF_Style(projectJsonContent, projectElement);
+  //console.log("1" + projectElement)
+  projectElement.querySelector("#project_description").textContent =
+    projectJsonContent.project_description;
+  projectElement.querySelector("#repo-image").src =
+    projectJsonContent.image_uri;
+  //console.log("2" + projectElement)
+
+  GenerateLinkButtons(projectJsonContent, projectElement);
+  document.getElementById("repo-content").appendChild(projectElement);
 }
 
 // generate tag items in the style of stack overflow
 function GenerateTagItems_SOF_Style(contentObject, mainDivClone) {
-    var tagTemplate = document.getElementById("TemplateTag");
-    for (var i = 0; i < contentObject.tags.length; i++) {
-        var tagClone = tagTemplate.content.cloneNode(true);
-        tagClone.querySelector("#tag_name").textContent = contentObject.tags[i].tag_name;
-        mainDivClone.querySelector(".tag-list").appendChild(tagClone);
-    }
+  var tagTemplate = document.getElementById("TemplateTag");
+  for (var i = 0; i < contentObject.tags.length; i++) {
+    var tagClone = tagTemplate.content.cloneNode(true);
+    tagClone.querySelector("#tag_name").textContent =
+      contentObject.tags[i].tag_name;
+    mainDivClone.querySelector(".tag-list").appendChild(tagClone);
+  }
 }
 
 // generate html code for link buttons
 function GenerateLinkButtons(contentObject, mainDivClone) {
-    var linkButtonTemplate = document.getElementById("TemplateRepoLink")
-    for (var key in contentObject.links) {
-        var linkButtonClone = linkButtonTemplate.content.cloneNode(true);
-        linkButtonClone.querySelector(".link-button").href = contentObject.links[key].link_href;
-        linkButtonClone.querySelector(".link-button").textContent = contentObject.links[key].link_name;
-        // add link to main div
-        mainDivClone.querySelector(".link-buttons").appendChild(linkButtonClone);
-    }
+  var linkButtonTemplate = document.getElementById("TemplateRepoLink");
+  for (var key in contentObject.links) {
+    var linkButtonClone = linkButtonTemplate.content.cloneNode(true);
+    linkButtonClone.querySelector(".link-button").href =
+      contentObject.links[key].link_href;
+    linkButtonClone.querySelector(".link-button").textContent =
+      contentObject.links[key].link_name;
+    // add link to main div
+    mainDivClone.querySelector(".link-buttons").appendChild(linkButtonClone);
+  }
 }
 
-// generate html from the projects.json file 
+// generate html from the projects.json file
 function GenerateProjectsHtml(projectJson) {
-    var projects = projectJson.projects
-    for (var i = 0; i < projects.length; i++) {
-        console.log("------\n" + projects[i].project_name + "\n")
-        CreateProjectHtml(projects[i], (i + startWithEven) % 2 == 0)
-    }
+  var projectNames = projectJson.projects;
+  for (var i = 0; i < projectNames.length; i++) {
+    console.log(projectNames[i]);
+    CreateProjectHtml(projectNames[i], (i + startWithEven) % 2 == 0);
+  }
 }
 
 function HttpGet(theUrl) {
-    var xmlHttp = new XMLHttpRequest()
-    xmlHttp.open("GET", theUrl, false) // false for synchronous request
-    xmlHttp.send(null)
-    return xmlHttp.responseText
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", theUrl, false); // false for synchronous request
+  xmlHttp.send(null);
+  return xmlHttp.responseText;
 }
 
-
-$(document).ready(function() {
-    var projectsJsonFile = HttpGet(reposPath + projectsPath)
-        // base64 decode the content-tag in the projects.json file from github
-    var projectJsonContent = atob(JSON.parse(projectsJsonFile).content)
-        // generate project elements with the parsed content-string
-    GenerateProjectsHtml(JSON.parse(projectJsonContent))
-})
+$(document).ready(function () {
+  var projectsJsonFile = HttpGet(reposPath + projectsPath + "projectsGP.json");
+  // base64 decode the content-tag in the projects.json file from github
+  var projectJsonContent = JSON.parse(
+    atob(JSON.parse(projectsJsonFile).content)
+  );
+  // generate project elements with the parsed content-string
+  GenerateProjectsHtml(projectJsonContent);
+});
